b90f0555a71e8e7dde558e24912ce3df2c62219b
jorgezaccaro <jorgezaccaro@gmail.com>
2017-05-07 13:55:31 +0800

    feat(index): refactored interface and initialization

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 'use strict'
 
-const port = process.env.PORT || 3000
-
 const Microapi = require('../../microapi/koa')
-const api = new Microapi()
+const Graphstore = require('./lib/clients/graphstore')
+const Hashtable = require('./lib/clients/hashtable')
 
-api.define('./api')
-api.listen(port)
+function databaseMiddleware(databases) {
+  return async (context, next) => {
+    context.database = databases
+    await next()
+  }
+}
 
-module.exports = api
+async function init({port = 3000} = {}) {
+  const [api, graphstore, hashtable] = await Promise.all([
+    new Microapi(),
+    Graphstore.database.connect(),
+    Hashtable.database.connect()
+  ])
+
+  api.use(databaseMiddleware({graphstore, hashtable}))
+  api.define('./api')
+  api.listen(port)
+
+  return {api, graphstore, hashtable}
+}
+
+if (require.main === module) {
+  init({port: process.env.PORT})
+}
+
+exports.init = init
