1ae103aecdda41d4a1c27a8b4bfe25fb57ad8dad
jorgezaccaro <jorgezaccaro@gmail.com>
2017-06-04 19:01:58 +0800

    fix(schemas): imported definitions due to arrays bug

--- api/definitions/transaction-inputs.js
+++ api/definitions/transaction-inputs.js
@@ -4,10 +4,14 @@ const joi = require('joi')
 const config = require('../config.json')
 const inputs = config.items.transaction.inputs
 
+const cryptoHashObject = require('./crypto-hash-object')
+const transactionIouData = require('./transaction-iou-data')
+const cryptoSignatures = require('./crypto-signatures')
+
 const items = joi.object().keys({
-  hash: joi.object({}).meta({className: 'crypto-hash-object'}).required(),
-  data: joi.object({}).meta({className: 'transaction-iou-data'}).required(),
-  sigs: joi.object({}).meta({className: 'crypto-signature-object'}).required()
+  hash: cryptoHashObject.meta({className: 'crypto-hash-object'}).required(),
+  data: transactionIouData.meta({className: 'transaction-iou-data'}).required(),
+  sigs: cryptoSignatures.meta({className: 'crypto-signatures'}).required()
 })
 
 const schema = joi.array().items(items)
