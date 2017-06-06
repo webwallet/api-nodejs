721f0ba058b20e36c3140e532531e148dcb70b81
jorgezaccaro <jorgezaccaro@gmail.com>
2017-06-06 18:01:04 +0800

    feat(transaction): updated outputs using inputs and previous outputs

--- api/routes/transaction/post.js
+++ api/routes/transaction/post.js
@@ -3,19 +3,21 @@
 const utils = require('*lib/utils')
 
 module.exports = async function handler({ request, params, database }) {
-  let transaction = {hash:{}, data: {}}
-  let data = request.body.data
+  let inputs = request.body.data.inputs
 
   /* 1. Extract wallet properties from input sources and targets */
-  let wallets = utils.iou.parseWalletProperties(data.inputs)
+  let wallets = utils.iou.parseWalletProperties(inputs)
   /* 2. Get public keys for signature verification, if applicable */
   /* 3. Verify digital signatures of all IOUs */
 
-  /* 4. Get unspent transaction outputs */
-  let outputs = await utils.transaction.getUnspentOutputs({wallets, database})
-  /* 5. Compute new outputs */
-  transaction.outputs = utils.transaction.buildOutputsArray(wallets)
-  /* 6. Build transaction document */
+  /* 4. Get previous unspent outputs and initialize new transaction outputs */
+  let previous = await utils.transaction.getUnspentOutputs({wallets, database})
+  let outputs = utils.transaction.buildOutputMapping(wallets)
+  /* 5. Combine inputs and previous outputs to compute new outputs */
+  await utils.transaction.feedPreviousToOutputs(previous, outputs)
+  await utils.transaction.feedInputsToOutputs(inputs, outputs)
+  /* 6. Generate and validate transaction document */
+
 
   /* 7. Send graphstore clearing query */
   /* 8. Run post-query validations */
