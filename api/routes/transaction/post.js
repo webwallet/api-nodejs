575e2fbf1020268a63680240b2be38e6050d8f4a
jorgezaccaro <jorgezaccaro@gmail.com>
2017-06-06 14:59:14 +0800

    feat(transaction): put all output retrieval ops in getUnspentOutputs

--- api/routes/transaction/post.js
+++ api/routes/transaction/post.js
@@ -3,23 +3,18 @@
 const utils = require('*lib/utils')
 
 module.exports = async function handler({ request, params, database }) {
+  let transaction = {hash:{}, data: {}}
   let data = request.body.data
+
   /* 1. Extract wallet properties from input sources and targets */
   let wallets = utils.iou.parseWalletProperties(data.inputs)
   /* 2. Get public keys for signature verification, if applicable */
   /* 3. Verify digital signatures of all IOUs */
 
   /* 4. Get unspent transaction outputs */
-  let pointers = await utils.transaction
-    .getUnspentPointers({wallets, database, inputs: data.inputs})
-  pointers.map(({address, pointers}) => wallets.get(address).pointers = pointers)
-
-  let contents = await utils.transaction.getOutputContents({wallets, database})
-  contents.map(({pointer, content}) => {
-    if (content.adr) wallets.get(content.adr).pointers.set(pointer, content)
-  })
-
+  let outputs = await utils.transaction.getUnspentOutputs({wallets, database})
   /* 5. Compute new outputs */
+  transaction.outputs = utils.transaction.buildOutputsArray(wallets)
   /* 6. Build transaction document */
 
   /* 7. Send graphstore clearing query */
