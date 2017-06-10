8527f3a19b1bc0d5cd50f179b52b4d3dfca06625
jorgezaccaro <jorgezaccaro@gmail.com>
2017-06-10 14:57:20 +0800

    feat(routes): transaction generation, storage and clearing

--- api/routes/transaction/post.js
+++ api/routes/transaction/post.js
@@ -6,7 +6,7 @@ module.exports = async function handler({ request, params, database }) {
   let inputs = request.body.data.inputs
 
   /* 1. Extract wallet properties from input sources and targets */
-  let wallets = utils.iou.parseWalletProperties(inputs)
+  let {wallets, countspaces} = utils.iou.parseWalletProperties(inputs)
   /* 2. Get public keys for signature verification, if applicable */
   /* 3. Verify digital signatures of all IOUs */
 
@@ -17,11 +17,20 @@ module.exports = async function handler({ request, params, database }) {
   await utils.transaction.feedPreviousToOutputs(previous, outputs)
   await utils.transaction.feedInputsToOutputs(inputs, outputs)
   /* 6. Generate and validate transaction document */
+  let transaction = utils.transaction.generateDocument({inputs})
+  transaction.data.outputs = utils.convertMapToArray(outputs)
+  transaction.hash().sign([])
 
-
-  /* 7. Send graphstore clearing query */
-  /* 8. Run post-query validations */
+  /* 7. Store transaction document in the hashtable database */
+  let hashtableResult = await utils.transaction
+    .storeTransactionRecord({transaction, database})
+  /* 8. Send graphstore clearing query and run post-query validations */
+  let queryParams = utils.transaction.buildQueryParameters(transaction)
+  queryParams.countspaces = countspaces
+  let graphQuery = await database.graphstore
+    .query('spendTransactionOutputs', queryParams)
   /* 9. commit / rollback */
+  graphQuery.transaction.commit()
 
   return {body: 'post /transaction', status: 200}
 }
