bd786d61bc18dd11292a0d57f059ca5e00364775
jorgezaccaro <jorgezaccaro@gmail.com>
2017-10-16 17:49:47 +0800

    feat(env): added environment variables for hashtable service

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function init({port = 3000} = {}) {
   const [api, graphstore, hashtable] = await Promise.all([
     new Microapi(),
     Graphstore.database.connect({host: process.env.GRAPHHOST, auth: {password: process.env.GRAPHPASS}}),
-    Hashtable.database.connect({host: process.env.COUCHHOST, name: 'hashtable', auth: {password: process.env.COUCHPASS}})
+    Hashtable.database.connect({host: process.env.COUCHHOST, name: process.env.COUCHNAME, auth: {password: process.env.COUCHPASS}})
   ])
 
   api.use(databaseMiddleware({graphstore, hashtable}))
