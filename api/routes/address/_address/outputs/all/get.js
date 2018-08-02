ca6b4cd88ce065df2f18c0a027244df1530d3a68
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-08-02 22:25:22 +0800

    added endpoint for retrieving transaction history

---
+++ api/routes/address/_address/outputs/all/get.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const utils = require('*lib/utils')
+
+async function handler({ request, params, database }) {
+  let wallets = new Map()
+  wallets.set(params.address, {countspaces: [request.query.unit]})
+
+  /* Get unspent transaction outputs in the given counstpaces */
+  let outputs = await utils.transaction.getPreviousOutputs({wallets, database})
+
+  return {body: {data: {outputs}}, status: 200}
+}
+
+module.exports = handler
