ecc59e46b0c8a5bd9250d51addcabe316f112edc
Jorge Zaccaro <jorgezaccaro@gmail.com>
2018-10-10 13:13:01 +0800

    feat(definitions): used @webwallet/schemas to replace local schemas

--- api/definitions/crypto-public-key.js
+++ api/definitions/crypto-public-key.js
@@ -1,10 +1,6 @@
 'use strict'
 
-const joi = require('joi')
-const config = require('../config.json')
-const {min, max} = config.lengths.crypto.publicKey
-
-const schema = joi.string().hex().min(min).max(max)
-  .description('cryptographic public key in hex format')
+const schemas = require('@webwallet/schemas').joi
+const schema = schemas.crypto.keys.public.string
 
 module.exports = schema
